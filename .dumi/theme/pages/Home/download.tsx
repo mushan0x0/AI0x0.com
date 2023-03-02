39b2fce58a2732b73cd4462f7cb6825dcb00b281
mushan0x0 <mushan0x0@gmail.com>
2023-03-02 09:46:15 +0800

    chore: use ali oss

--- .dumi/theme/pages/Home/download.tsx
+++ .dumi/theme/pages/Home/download.tsx
@@ -1,23 +1,27 @@
 import axios from 'axios';
+import YAML from 'yaml';
+
+const platform = navigator.platform.toLowerCase();
+const osName = /win/.test(platform)
+  ? 'win'
+  : /mac/.test(platform)
+  ? 'mac'
+  : 'linux';
 
 // 获取 Git 仓库最新版本信息
-async function getLatestVersion(repoUrl: string): Promise<string> {
+async function getLatestVersion(): Promise<string> {
   const response = await axios.get(
-    `https://api.github.com/repos/${repoUrl}/releases/latest`,
+    `https://bojuematerial-prudcut-public.oss-cn-guangzhou.aliyuncs.com/other/versions/latest-${osName}.yml`,
   );
-  return response.data.tag_name;
+  return YAML.parse(response.data).version;
 }
 
 // 获取适合当前平台的下载链接
 function getDownloadLink(downloadUrls: Record<string, string>): string {
-  const platform = navigator.platform.toLowerCase();
-  const osName = /win/.test(platform)
-    ? 'win'
-    : /mac/.test(platform)
-    ? 'mac'
-    : 'linux';
-  if (osName === 'linux') {
-    alert('暂不支持linux，敬请期待');
+  if (osName !== 'mac') {
+    alert(
+      '目前仅支持Mac，您的系统即将上线，可以点击网页下方与我联系参与内测！',
+    );
     return '';
   }
 
@@ -35,14 +39,13 @@ function downloadFile(url: string): void {
 
 // 示例用法
 export async function downloadLatestPackage() {
-  const repoUrl = 'mushan0x0/ai0x0.com';
-  const version = (await getLatestVersion(repoUrl)).replace('v', '');
+  const version = (await getLatestVersion()).replace('v', '');
   const downloadUrls = {
     // win: `https://github.com/${repoUrl}/releases/download/${version}/AI-0x0_${version.replace('v', '')}.exe`,
     // mac: `https://github.com/${repoUrl}/releases/download/${version}/AI-0x0_${version.replace('v', '')}.dmg`,
     // linux: `https://github.com/${repoUrl}/releases/download/${version}/AI-0x0_${version.replace('v', '')}-linux-x64.tar.gz`,
-    win: `https://bojuematerial-prudcut-public.oss-cn-guangzhou.aliyuncs.com/other/versions/AI-0x0_${version}.exe`,
-    mac: `https://bojuematerial-prudcut-public.oss-cn-guangzhou.aliyuncs.com/other/versions/AI-0x0_${version}.dmg`,
+    win: `https://bojuematerial-prudcut-public.oss-cn-guangzhou.aliyuncs.com/other/versions/AI%200x0_${version}.exe`,
+    mac: `https://bojuematerial-prudcut-public.oss-cn-guangzhou.aliyuncs.com/other/versions/AI%200x0_${version}.dmg`,
     // linux: `https://github.com/${repoUrl}/releases/download/${version}/AI-0x0_${version.replace('v', '')}-linux-x64.tar.gz`,
   };
   const downloadLink = getDownloadLink(downloadUrls);
